5c0244eaa02b85a4d8bfab5f4c92adda08ba9f55
JanakiAR <ajayramanjanaki@kpit.com>
2018-04-07 11:53:00 +0530

    Initial CheckIn v1

---
+++ TraitMigration/src/ext/x28/service/EntryPoint.java
@@ -0,0 +1,22 @@
+package ext.x28.service;
+/**
+ * 
+ * @author janakiar
+ *
+ */
+public class EntryPoint {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("x28 EntryPoint");
+		/**
+		 * 
+		 */
+		System.out.println("Second Test");
+	}
+
+}
